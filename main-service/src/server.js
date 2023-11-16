7a81e3e34e436041635d60a86c3fb9f587cf6766
itsRoy04 <mkshr8@gmail.com>
2023-11-16 13:30:07 +0530

    latest

--- main-service/src/server.js
+++ main-service/src/server.js
@@ -5,9 +5,9 @@ const app  = express();
 app.get('/', (req, res) => {
     console.log("Main service called");
 
-    res.send("Root Service asdasdasdaasdasdasdsd!");
+    res.send("Root Service asdase dfsdfd asdasd!");
 });
 
 app.listen(3000, () => {
-    console.log('User service started on port 3000')
+    console.log('Main service started on port 3000')
 })
