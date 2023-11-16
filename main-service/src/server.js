1f1c322712b61eae63a8a2b9cce1c35178d9dccc
itsRoy04 <mkshr8@gmail.com>
2023-11-16 15:25:03 +0530

    cookie parser removed

--- main-service/src/server.js
+++ main-service/src/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const app  = express();
 
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
 app.use(cors(corsOptions));
 
 app.use(express.json())
