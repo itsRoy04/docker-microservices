b9d9344159d2e63f3210f9f30fecf0b2e5ce36d0
itsRoy04 <mkshr8@gmail.com>
2023-11-22 18:56:59 +0530

    Main Root Service Updated for testing

--- main-service/src/server.js
+++ main-service/src/server.js
@@ -5,7 +5,7 @@ const app  = express();
 app.get('/', (req, res) => {
     console.log("Main service called");
 
-    res.send("Root Service Jenkins! Checking");
+    res.send("Main Root Service Jenkins! Checking");
 });
 
 app.listen(3000, () => {
