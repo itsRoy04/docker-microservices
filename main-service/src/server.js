11d04b3441522b53416cd4942a026f25b304c4bd
Rajarshi Mandal <rajarshim5@gmail.com>
2023-11-22 14:33:33 +0530

    Update server.js

--- main-service/src/server.js
+++ main-service/src/server.js
@@ -5,7 +5,7 @@ const app  = express();
 app.get('/', (req, res) => {
     console.log("Main service called");
 
-    res.send("ROTI Servvvice asdasdddddssasdasdasddd!");
+    res.send("ROOT Servvvice asdasdddddssasdasdasddd!");
 });
 
 app.listen(3000, () => {
