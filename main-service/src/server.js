0eb0cced6c9ddcebf0b86577d7d4bb2650d32fe1
itsRoy04 <mkshr8@gmail.com>
2023-11-22 18:24:33 +0530

    Updating main for testing

--- main-service/src/server.js
+++ main-service/src/server.js
@@ -5,9 +5,9 @@ const app  = express();
 app.get('/', (req, res) => {
     console.log("Main service called");
 
-    res.send("ROOTtt Servvvice Working with Jenkins!");
+    res.send("Root Service Working with Jenkins!");
 });
 
 app.listen(3000, () => {
     console.log('Main service started on port 3000')
-})
+}) 
