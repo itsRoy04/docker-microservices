9f8b2cc1075b3358fa5e712760f6b1fcf014b3ec
Rajarshi Mandal <rajarshim5@gmail.com>
2023-11-23 10:15:46 +0530

    Test for jenkins

--- main-service/src/server.js
+++ main-service/src/server.js
@@ -5,7 +5,7 @@ const app  = express();
 app.get('/', (req, res) => {
     console.log("Main service called");
 
-    res.send("Main Root Service Jenkins! Checking");
+    res.send("Main Root Service Jenkins! Checking 2");
 });
 
 app.listen(3000, () => {
